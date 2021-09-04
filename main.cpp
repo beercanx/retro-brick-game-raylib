785fa777c6ba502799c1a1d034a4530094ed65de
James Bacon <james@baconi.co.uk>
2021-09-04 19:22:55 +0100

    Added RayLib and a basic test of the library.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,65 @@
 #include <iostream>
+#include <raylib.h>
 
 int main() {
+
     std::cout << "Hello, World!" << std::endl;
+
+    // Window Dimensions
+    int windowWidth{800};
+    int windowHeight{450};
+
+    InitWindow(windowWidth, windowHeight, "Axe Game");
+
+    // Circle
+    int circleX{windowWidth / 2};
+    int circleY{windowHeight / 2};
+    int circleRadius{25};
+
+    // Axe
+    int axeX{300};
+    int axeY{0};
+    int axeWidth{50};
+    int axeHeight{50};
+    int axeDirection{10};
+
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+
+        ClearBackground(WHITE);
+
+        // Draw Circle
+        DrawCircle(circleX, circleY, circleRadius, BLUE);
+
+        // Draw Axe
+        DrawRectangle(axeX, axeY, axeWidth, axeHeight, RED);
+
+        // Move the Axe
+        axeY += axeDirection;
+        if (axeY + axeHeight > windowHeight || axeY < 0) {
+            axeDirection = -axeDirection;
+        }
+
+        // Do X Axis Movement
+        if (IsKeyDown(KEY_D) && circleX + circleRadius < windowWidth) {
+            circleX += circleRadius;
+        }
+        if (IsKeyDown(KEY_A) && circleX - circleRadius > 0) {
+            circleX -= circleRadius;
+        }
+
+        // Do Y Axis Movement
+        if (IsKeyDown(KEY_W) && circleY - circleRadius > 0) {
+            circleY -= circleRadius;
+        }
+        if (IsKeyDown(KEY_S) && circleY + circleRadius < windowHeight) {
+            circleY += circleRadius;
+        }
+
+        EndDrawing();
+    }
+
     return 0;
 }
