313cd4504f4650053a58298c130cd6b78d424338
James Bacon <james@baconi.co.uk>
2021-12-12 18:29:30 +0000

    Created a new sprite and the base class to start using it.

---
+++ src/bricks/SpriteBrick.h
@@ -0,0 +1,14 @@
+//
+// Created by Beercan on 12/12/2021.
+//
+
+#ifndef RETRO_BRICK_GAME_SPRITEBRICK_H
+#define RETRO_BRICK_GAME_SPRITEBRICK_H
+
+
+class SpriteBrick {
+
+};
+
+
+#endif //RETRO_BRICK_GAME_SPRITEBRICK_H
