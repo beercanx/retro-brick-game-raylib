8d88ec7adad1060ec96a71d96f006f41743d5d87
James Bacon <james@baconi.co.uk>
2023-10-07 16:46:24 +0100

    Added emscripten to CMake build scripts
    Refactored code to support running under emscripten

--- README.md
+++ README.md
@@ -80,14 +80,14 @@ This is for building in Windows to be able to run on Windows.
 Basically this is the normal build in Windows to be able to run on Windows type thing.
 
 1) Install MinGW natively on Windows, I use https://community.chocolatey.org
-```powershell
-choco install mingw
-```
+    ```powershell
+    choco install mingw
+    ```
 
 2) Create a toolchain using MinGW and set the environment to the location of mingw and let it do its detection magic, for me mingw got installed under:
-```
-C:\ProgramData\chocolatey\lib\mingw\tools\install\mingw64
-```
+    ```
+    C:\ProgramData\chocolatey\lib\mingw\tools\install\mingw64
+    ```
 
 3) Create a CMake profile that uses the MinGW Windows toolchain.
 
@@ -98,23 +98,23 @@ C:\ProgramData\chocolatey\lib\mingw\tools\install\mingw64
 Basically this is build in Linux (Ubuntu) to be able to run on Windows.
 
 1) Install MinGW in the WSL environment.
-```bash
-sudo apt install cmake mingw-w64 gdb-mingw-w64 
-```
+    ```bash
+    sudo apt install cmake mingw-w64 gdb-mingw-w64 
+    ```
 
 2) Manually create the toolchain instead of relying on the cmake auto-detection.
-```
-CMake:        /usr/bin/cmake
-Make:         /usr/bin/make
-C Compiler:   /usr/bin/x86_64-w64-mingw32-gcc
-C++ Compiler: /usr/bin/x86_64-w64-mingw32-g++
-Debugger:     /usr/bin/x86_64-w64-mingw32-gdb
-```
+    ```
+    CMake:        /usr/bin/cmake
+    Make:         /usr/bin/make
+    C Compiler:   /usr/bin/x86_64-w64-mingw32-gcc
+    C++ Compiler: /usr/bin/x86_64-w64-mingw32-g++
+    Debugger:     /usr/bin/x86_64-w64-mingw32-gdb
+    ```
 
 3) Create a CMake profile that uses the WSL MinGW tool chain and in the CMake options field add:
-```
--DCMAKE_TOOLCHAIN_FILE=CMake/x86_64-w64-mingw32.cmake
-```
+    ```
+    -DCMAKE_TOOLCHAIN_FILE=CMake/x86_64-w64-mingw32.cmake
+    ```
 
 4) Build, run and debug as normal now.
 
@@ -123,14 +123,14 @@ Debugger:     /usr/bin/x86_64-w64-mingw32-gdb
 Basically this is to build in Linux (Ubuntu) to be able to run on Linux.
 
 1) Install some stuff for building
-```bash
-sudo apt install cmake build-essential gdb-multiarch
-```
+    ```bash
+    sudo apt install cmake build-essential gdb-multiarch
+    ```
 
 2) Install some stuff for Raylib dependencies
-```bash
-sudo apt install libasound2-dev mesa-common-dev libx11-dev libxrandr-dev libxi-dev xorg-dev libgl1-mesa-dev libglu1-mesa-dev
-```
+    ```bash
+    sudo apt install libasound2-dev mesa-common-dev libx11-dev libxrandr-dev libxi-dev xorg-dev libgl1-mesa-dev libglu1-mesa-dev
+    ```
 
 3) Create a toolchain using WSL and let the default defection to work as it should pick up the Linux GNU commands.
 
@@ -139,3 +139,20 @@ sudo apt install libasound2-dev mesa-common-dev libx11-dev libxrandr-dev libxi-d
 5) Update the run configuration for the GUI app you want to build and run from WSL to include the DISPLAY value for your machine. I could get away with setting `DISPLAY=%HOSTNAME%.mshome.net:0`, but basically it's the same value as laid out in [xlaunch/README.md](xlaunch/README.md).
 
 6) Build, run and debug as normal now.
+
+### Emscripten
+
+This is to build in order to run in a browser, these instructions focus on Windows, but should be possible on Linux too.
+
+1) Install Emscripten on Windows, I used https://community.chocolatey.org/packages/emscripten, there are more officially supported ways, but I'm lazy https://emscripten.org/docs/getting_started/downloads.html
+    ```powershell
+    choco install emscripten
+    ```
+ 
+2) Create a toolchain on the same platform as your Emscripten install and add in the CMake options field add:
+   ```
+   -DCMAKE_TOOLCHAIN_FILE=${EMSDK_HOME}/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake
+   -DPLATFORM=Web
+   ```
+   
+3) Build as normal now but remember it is going to create a HTML page, which you can't launch like an executable.
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <list>
+#include <memory>
 #include "raylib.h"
 #include "bricks/RawBrick.h"
 #include "bricks/Enemy.h"
@@ -11,6 +12,31 @@
 #include "raylib/Vector2.h"
 #include "sidebar/Level.h"
 #include "GameView.h"
+#if defined(PLATFORM_WEB)
+#include <emscripten/emscripten.h>
+#endif
+
+// Game Area
+Background background{Brick::space * 2};
+const GameView gameView = background.gameView;
+
+// Bricks
+std::unique_ptr<Enemy> enemy;
+std::unique_ptr<Player> player;
+
+// Bullets
+std::list<Bullet> bullets{};
+
+// Sidebar
+GameOver gameOver{gameView.innerTopLeft + Vector2{0, gameView.outer.height / 3}};
+Score score{gameView.outerTopRight + Brick::right};
+Level level{ gameView.outerTopRight + Brick::right + Brick::down};
+
+// Pause
+float pausedTrigger{0.0f};
+bool paused{true};
+
+void UpdateDrawFrame();
 
 int main() {
 
@@ -28,168 +54,156 @@ int main() {
     // Textures
     const Texture2D spriteTexture{LoadTexture("assets/bricks/sprite.png")};
 
-    // Game Area
-    Background background{Brick::space * 2};
-    const GameView gameView = background.gameView;
-
     // Bricks
-    Enemy enemy{
+    enemy = std::make_unique<Enemy>(
         spriteTexture,
         Enemy::Type::seat,
         gameView
-    };
-    Player player{
+    );
+    player = std::make_unique<Player>(
         spriteTexture,
         gameView.innerTopLeft + (Brick::down * 13) + (Brick::right * 3) - Brick::space,
         gameView
-    };
-
-    // Bullets
-    std::list<Bullet> bullets{};
-
-    // Sidebar
-    Score score{gameView.outerTopRight + Brick::right};
-    Level level{ gameView.outerTopRight + Brick::right + Brick::down};
-    GameOver gameOver{gameView.innerTopLeft + Vector2{0, gameView.outer.height / 3}};
+    );
 
     // Register event hooks
-    level.onLevelChange.emplace_back([&background](int) -> void { background.resetBackground(); });
-    level.onScoreScaleChange.emplace_back([&score](int scale) -> void { score.scoreScale = scale; });
-    level.onSpeedScaleChange.emplace_back([&enemy](int scale) -> void { enemy.speedScale = scale; });
-    level.onSpeedScaleChange.emplace_back([&background](int scale) -> void { background.speedScale = scale; });
+    level.onLevelChange.emplace_back([&](int) -> void { background.resetBackground(); });
+    level.onScoreScaleChange.emplace_back([&](int scale) -> void { score.scoreScale = scale; });
+    level.onSpeedScaleChange.emplace_back([&](int scale) -> void { enemy->speedScale = scale; });
+    level.onSpeedScaleChange.emplace_back([&](int scale) -> void { background.speedScale = scale; });
 
-    // Pause
-    float pausedTrigger{0.0f};
-    bool paused{true};
-
+#if defined(PLATFORM_WEB)
+    emscripten_set_main_loop(UpdateDrawFrame, 0, 1);
+#else
     SetTargetFPS(60);
-
-    while (!WindowShouldClose()) {
-
-        const float deltaTime = GetFrameTime();
-
-        BeginDrawing();
-        ClearBackground(WHITE);
-
-        pausedTrigger += deltaTime;
-        if(IsKeyDown(KEY_P) && pausedTrigger >= 0.1f) {
-            paused = !paused;
-            pausedTrigger = 0.0f;
-        }
-
-        // TODO - Draw the non game area:
-        //          Score -- DONE
-        //          Level -- DONE
-        //          Music -- Space to display if sound and music is enabled
-        //          Game Over -- Space for Game Over text
-        //          Next -- Text
-        //          Next Shape - Space for a next shape view (Title + 4 by 4 grid)
-        //          Speed -- Text
-        //          Speed -- Space to display current speed (2 digits, maybe base speed)
-        //          Level -- Text
-        //          Level -- Space to display current level (2 digits)
-        //          Paused -- Space to display a pause symbol (maybe a cup tea/coffee)
-
-        // TODO - Add non game buttons, starting with a pause, sound and music toggles
-
-        if(!paused) {
-
-            background.handleMovement(deltaTime);
-
-            // Handle player movement
-            player.handleMovement(deltaTime);
-
-            // Handle enemy movement
-            enemy.handleMovement(deltaTime);
-
-            // Handle enemy colliding with player
-            if (CheckCollisionRecs(player.getDestination(), enemy.getDestination())) {
-                std::cout << "Player was killed by an Enemy" << std::endl;
-
-                // Stop moving the background
-                background.active = false;
-
-                // Kill the player
-                player.handleDeath();
-
-                // Kill the enemy too?
-                enemy.handleDeath();
-
-                // Display game over
-                gameOver.display();
-            }
-
-            // Handle bullet movement and collisions
-            for (auto bullet = bullets.begin(); bullet != bullets.end(); bullet++) {
-
-                // Handle bullet movement
-                bullet->updatePosition(bullet->position + Brick::up);
-
-                // Handle collisions with enemies.
-                if (CheckCollisionRecs(bullet->getDestination(), enemy.getDestination())) {
-                    std::cout << "Bullet has collided with Enemy" << std::endl;
-                    level.updateProgress(1);
-                    score.increase();
-                    bullets.erase(bullet);
-                    enemy.handleReBirth();
-                }
-
-                // Remove bullet from game once invisible.
-                if (bullet->position.y < gameView.outerTopLeft.y) {
-                    std::cout << "Bullet has been deleted" << std::endl;
-                    bullets.erase(bullet);
-                }
-            }
-
-            // Handler player shooting
-            if (const auto &bullet = player.handleShooting(deltaTime)) {
-                bullets.push_back(*bullet);
-            }
-        }
-
-        // Draw bricks
-        background.draw();
-        enemy.draw();
-        player.draw();
-        for(const auto& bullet : bullets) {
-            bullet.draw();
-        }
-
-        // Draw Sidebar
-        score.draw();
-        level.draw();
-        gameOver.draw();
-        // TODO - Draw a level progress bar?
-        // TODO - Draw next incoming shape
-        if(paused) {
-            DrawText(
-                "Paused",
-                (int) gameView.inner.width / 3,
-                (int) gameView.inner.height / 3,
-                10 * Brick::scale,
-                RED
-            );
-        }
-
-        // Gizmos
-//        DrawRectangleLinesEx(gameView.outer, 1.0f, RED);
-//        DrawLineV(gameView.outerTopLeft, gameView.outerTopRight, RED);
-//        DrawLineV(gameView.outerTopRight, gameView.outerBottomRight, RED);
-//        DrawLineV(gameView.outerBottomRight, gameView.outerBottomLeft, RED);
-//        DrawLineV(gameView.outerBottomLeft, gameView.outerTopLeft, RED);
-
-//        DrawRectangleLinesEx(gameView.inner, 1.0f, PURPLE);
-//        DrawLineV(gameView.innerTopLeft, gameView.innerTopRight, PURPLE);
-//        DrawLineV(gameView.innerTopRight, gameView.innerBottomRight, PURPLE);
-//        DrawLineV(gameView.innerBottomRight, gameView.innerBottomLeft, PURPLE);
-//        DrawLineV(gameView.innerBottomLeft, gameView.innerTopLeft, PURPLE);
-
-        EndDrawing();
-    }
+    while (!WindowShouldClose()) UpdateDrawFrame(); // Detect window close button or ESC key
+#endif
 
     UnloadTexture(spriteTexture);
 
     CloseWindow();
 
     return 0;
+}
+
+void UpdateDrawFrame() {
+    const float deltaTime = GetFrameTime();
+
+    BeginDrawing();
+    ClearBackground(WHITE);
+
+    pausedTrigger += deltaTime;
+    if(IsKeyDown(KEY_P) && pausedTrigger >= 0.1f) {
+        paused = !paused;
+        pausedTrigger = 0.0f;
+    }
+
+    // TODO - Draw the non game area:
+    //          Score -- DONE
+    //          Level -- DONE
+    //          Music -- Space to display if sound and music is enabled
+    //          Game Over -- Space for Game Over text
+    //          Next -- Text
+    //          Next Shape - Space for a next shape view (Title + 4 by 4 grid)
+    //          Speed -- Text
+    //          Speed -- Space to display current speed (2 digits, maybe base speed)
+    //          Level -- Text
+    //          Level -- Space to display current level (2 digits)
+    //          Paused -- Space to display a pause symbol (maybe a cup tea/coffee)
+
+    // TODO - Add non game buttons, starting with a pause, sound and music toggles
+
+    if(!paused) {
+
+        background.handleMovement(deltaTime);
+
+        // Handle player movement
+        player->handleMovement(deltaTime);
+
+        // Handle enemy movement
+        enemy->handleMovement(deltaTime);
+
+        // Handle enemy colliding with player
+        if (CheckCollisionRecs(player->getDestination(), enemy->getDestination())) {
+            std::cout << "Player was killed by an Enemy" << std::endl;
+
+            // Stop moving the background
+            background.active = false;
+
+            // Kill the player
+            player->handleDeath();
+
+            // Kill the enemy too?
+            enemy->handleDeath();
+
+            // Display game over
+            gameOver.display();
+        }
+
+        // Handle bullet movement and collisions
+        for (auto bullet = bullets.begin(); bullet != bullets.end(); bullet++) {
+
+            // Handle bullet movement
+            bullet->updatePosition(bullet->position + Brick::up);
+
+            // Handle collisions with enemies.
+            if (CheckCollisionRecs(bullet->getDestination(), enemy->getDestination())) {
+                std::cout << "Bullet has collided with Enemy" << std::endl;
+                level.updateProgress(1);
+                score.increase();
+                bullets.erase(bullet);
+                enemy->handleReBirth();
+            }
+
+            // Remove bullet from game once invisible.
+            if (bullet->position.y < gameView.outerTopLeft.y) {
+                std::cout << "Bullet has been deleted" << std::endl;
+                bullets.erase(bullet);
+            }
+        }
+
+        // Handler player shooting
+        if (const auto &bullet = player->handleShooting(deltaTime)) {
+            bullets.push_back(*bullet);
+        }
+    }
+
+    // Draw bricks
+    background.draw();
+    enemy->draw();
+    player->draw();
+    for(const auto& bullet : bullets) {
+        bullet.draw();
+    }
+
+    // Draw Sidebar
+    score.draw();
+    level.draw();
+    gameOver.draw();
+    // TODO - Draw a level progress bar?
+    // TODO - Draw next incoming shape
+    if(paused) {
+        DrawText(
+            "Paused",
+            (int) gameView.inner.width / 3,
+            (int) gameView.inner.height / 3,
+            10 * Brick::scale,
+            RED
+        );
+    }
+
+    // Gizmos
+    //    DrawRectangleLinesEx(gameView.outer, 1.0f, RED);
+    //    DrawLineV(gameView.outerTopLeft, gameView.outerTopRight, RED);
+    //    DrawLineV(gameView.outerTopRight, gameView.outerBottomRight, RED);
+    //    DrawLineV(gameView.outerBottomRight, gameView.outerBottomLeft, RED);
+    //    DrawLineV(gameView.outerBottomLeft, gameView.outerTopLeft, RED);
+    //
+    //    DrawRectangleLinesEx(gameView.inner, 1.0f, PURPLE);
+    //    DrawLineV(gameView.innerTopLeft, gameView.innerTopRight, PURPLE);
+    //    DrawLineV(gameView.innerTopRight, gameView.innerBottomRight, PURPLE);
+    //    DrawLineV(gameView.innerBottomRight, gameView.innerBottomLeft, PURPLE);
+    //    DrawLineV(gameView.innerBottomLeft, gameView.innerTopLeft, PURPLE);
+
+    EndDrawing();
 }
