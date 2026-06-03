006b34d51c940e18b1a2f9dea67bbdc4f5d3cb68
James Bacon <james@baconi.co.uk>
2026-06-03 13:53:05 +0100

    Upgraded the Netty bom from 4.1.132 to 4.1.333

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ buildscript {
     // Review these on each update of the AGP (com.android.application)
     gradle.extra["securityBoms"] = listOf(
         "org.bouncycastle:bc-jdk18on-bom:1.84",
-        "io.netty:netty-bom:4.1.132.Final",
+        "io.netty:netty-bom:4.1.133.Final",
     )
     gradle.extra["securityPatches"] = listOf(
         "org.apache.httpcomponents:httpmime:4.5.14",
